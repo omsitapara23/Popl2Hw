6df0a884896dbef4d6f3cb4d9e7f09c3aa3f3456
Om Sitapara <omsitapara23@gmail.com>
2018-01-16 20:07:29 +0530

    added openMp parallelism

--- Popl2Hw/multithread.cpp
+++ Popl2Hw/multithread.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <omp.h>
 using namespace std;
 
 int result[100];
 int a[100], b[100];
 
 void adder(int i) {
-	result[i] = a[i] + b[i];
+	for (int j = i; j < 100; j = j + 2) {
+		result[j] = a[j] + b[j];
+	}
+}
+
+void print_result() {
+	cout << "The result array" << endl;
+	for (int i = 0; i < 100; i++) {
+		cout << result[i] << endl;
+	}
 }
 
 
@@ -28,9 +38,9 @@ int main() {
 	
 	cout << "Time for sequential  :" << chrono::duration_cast<chrono::microseconds>(end - start).count() << endl;
 
-	std::thread threads[100];
+	std::thread threads[2];
 	auto start1 = chrono::steady_clock::now();
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 2; i++) {
 		threads[i] = std::thread(adder, i);
 	}
 	for (auto &th : threads) th.join();
@@ -39,6 +49,19 @@ int main() {
 
 	cout << "Time for parallel  :" << chrono::duration_cast<chrono::microseconds>(end1 - start1).count() << endl;
 
+	auto start2 = chrono::steady_clock::now();
+#pragma omp parallel for
+	{
+		for (int i = 0; i < 100; i++) {
+			result[i] = a[i] + b[i];
+		}
+	}
+
+	auto end2 = chrono::steady_clock::now();
+
+	cout << "Time by openMp :" << chrono::duration_cast<chrono::microseconds>(end2 - start2).count() << endl;
+
+	//print_result();
 
 	system("pause");
 	return 0;
